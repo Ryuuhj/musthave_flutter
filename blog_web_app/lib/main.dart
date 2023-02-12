e49179657832faf70aed0256a429a209a6765c95
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-02-12 23:54:39 +0900

    init

---
+++ blog_web_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      //home => 앱이 처음 실행 됐을 때 보여줄 화면
+      home: HomeScreen(),
+    ),
+  );
+}
+
