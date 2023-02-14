393976a80891a8d64c774520009e5a75438675ab
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-02-14 18:20:44 +0900

    feat : PageView 등록, 상태바 변경 - PageView -> 이미지 위젯 스와이프 넘기기 - SystemChrome -> 상태바 관련 설정

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}):
@@ -6,8 +7,19 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) { //생명주기 차트에서 build() -> state dirty일 때 재실행
+    //상태바 색상 변경
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return Scaffold( //실행 결과로 위젯 반환
-      body: Text('Home Screen'),
+      body: PageView(
+        children: [1, 2, 3, 4, 5] //샘플 리스트 생성
+            .map( //리스트 별 각 위젯으로 매핑
+                (number) => Image.asset(
+                  'asset/img/image_$number.jpeg',
+                  fit: BoxFit.cover,
+                ), //매핑할 이미지 경로
+            ).toList(),
+      ),
     );
   }
 }
