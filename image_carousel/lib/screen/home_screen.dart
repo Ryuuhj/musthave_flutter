3500120a057d12265f2ce0a3eb68dc05d13bad8e
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-02-14 18:35:50 +0900

    feat : Timer 추가 - initState() 함수 추가 - 함수에 Timer.periodic() 추가

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,34 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-  //1. StatefulWidget 정의
+//1. StatefulWidget 정의
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}):
-        super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
-  //initState()
+  //initState() 아님, 그냥 State 클래스 생성
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
-  //2. _HomeScreenState 정의
+//2. _HomeScreenState 정의
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     //상태바 색상 변경
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
-    return Scaffold( //실행 결과로 위젯 반환
+    // initState() 함수 등록
+    @override
+    void initState() {
+      //부모 initState() 실행
+      super.initState();
+
+      //타이머 추가
+      Timer.periodic(
+        Duration(seconds: 3),
+        (timer) {
+          print('실행!');
+        },
+      );
+    }
+
+    return Scaffold(
+      //실행 결과로 위젯 반환
       body: PageView(
         children: [1, 2, 3, 4, 5] //샘플 리스트 생성
-            .map( //리스트 별 각 위젯으로 매핑
-                (number) => Image.asset(
-                  'asset/img/image_$number.jpeg',
-                  fit: BoxFit.cover,
-                ), //매핑할 이미지 경로
-            ).toList(),
+            .map(
+              //리스트 별 각 위젯으로 매핑
+              (number) => Image.asset(
+                'asset/img/image_$number.jpeg',
+                fit: BoxFit.cover,
+              ), //매핑할 이미지 경로
+            )
+            .toList(),
       ),
     );
   }
